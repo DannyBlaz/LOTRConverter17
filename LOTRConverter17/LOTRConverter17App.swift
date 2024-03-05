e78fe0a2a0d0bb4492cad8b38a5d4fb7413f0d6f
Daniel <dannyblaze469@gmail.com>
2024-03-04 21:18:21 -0800

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Daniel Ezekiel on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
